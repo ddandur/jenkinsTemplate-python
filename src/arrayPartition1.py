eb1b19df8cd567b0598a89d272be4e29c5c70b00
ddandur <daniel.r.dandurand@gmail.com>
2017-06-07 13:15:58 -0700

    added solution code

--- src/arrayPartition1.py
+++ src/arrayPartition1.py
@@ -5,7 +5,18 @@ class Solution(object):
         :rtype: int
         """
 
-        # your solution here
+        # sort list and group neighboring pairs;
+        # this means instead of smallest half of values being summed
+        # (worst case) the small values are eliminated as fast as possible
 
-        return 0
+        # this means the sum is every other number in sorted (min first) list,
+        # starting at a_0 and ending at one before end of list
 
+        # sort input
+        nums.sort()
+
+        # find max sum
+        summ = 0
+        for i in xrange(len(nums)/2):
+            summ += nums[2*i]
+        return summ
